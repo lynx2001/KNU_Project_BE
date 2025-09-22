1e9128bd15c07f8e6269b69b2cec39123c199fe0
lynx2001 <kangaroo125@naver.com>
2025-09-22 16:26:15 +0900

    First Commit  models.py

--- news/models.py
+++ news/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=200, null=False)
+    content = models.TextField(null=True, blank=True)
+    author = models.CharField(max_length=20)
+    url = models.CharField(max=255)
+    journal = models.CharField(max=50)
+    created_at = models.DateTimeField()
+
+    class Meta:
+        db_table = "article"
+
+    def __str__(self):
+        return self.title
