15d1e3676a4eaa49a0a44bd903f158adf9440bbb
lynx2001 <kangaroo125@naver.com>
2025-11-11 00:11:49 +0900

    return old

--- quiz/models.py
+++ quiz/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Quiz(models.Model):
     #id = models.BigAutoField(primary_key=True)
+    # 문제 난이도(사용자 등급에 따른) / 문제 해설 / 중복 여부
     question = models.TextField(null=False, blank=False)
     answer = models.TextField(null=False, blank=False)
     ox = models.BooleanField()
--- summary/models.py
+++ summary/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Summary(models.Model):
     #id = models.BigAutoField(primary_key=True)
+    # 신문 원문 요약 날짜 추가
     article_id = models.IntegerField(null=False)
     title = models.CharField(max_length=200, null=True)
     content = models.TextField(null=True, blank=True)
