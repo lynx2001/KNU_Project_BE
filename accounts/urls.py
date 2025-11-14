b95ce19b1f3db832296c0e0515301cae354005c2
lynx2001 <kangaroo125@naver.com>
2025-11-14 16:02:34 +0900

    account mistake error

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,8 +5,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     path("signup/", UserSignupView.as_view(), name="signup"),
     path("login/", TokenObtainPairView.as_view(), name="login"),  # JWT 로그인
-    path("/logout/", LogoutView.as_view(), name="logout"),
-    path("/password-change/", PasswordChangeView.as_view(), name="password_change"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("password-change/", PasswordChangeView.as_view(), name="password_change"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("profile/", ProfileView.as_view(), name="profile"),  # 인증 필요
 ]
